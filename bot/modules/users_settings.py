852a25e1362201898bd546a947caad33459d75e6
5hojib <yesiamshojib@gmail.com>
2025-02-11 06:11:22 +0000

    Auto-format code [skip actions]

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -11,8 +11,8 @@ from pyrogram.filters import create
 from pyrogram.handlers import MessageHandler
 
 from bot import auth_chats, excluded_extensions, sudo_users, user_data
-from bot.core.config_manager import Config
 from bot.core.aeon_client import TgClient
+from bot.core.config_manager import Config
 from bot.helper.ext_utils.bot_utils import (
     get_size_bytes,
     new_task,
