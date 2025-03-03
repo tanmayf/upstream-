3ec8e253f6d55b3ccd09f57b80758c9e9a5876a3
5hojib <yesiamshojib@gmail.com>
2025-03-03 22:59:28 +0000

    format: Auto-format code by ruff [skip actions].

--- bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
@@ -331,7 +331,7 @@ def devuploads(url):
         raise DirectDownloadLinkException("ERROR: Unable to find xd value")
     data["xd"] = randpost.text.strip()
     proxy = "http://hsakalu2:hsakalu2@45.151.162.198:6600"
-    res = session.post(url, data=data, proxies={'http': proxy, 'https': proxy})
+    res = session.post(url, data=data, proxies={"http": proxy, "https": proxy})
     html = HTML(res.text)
     if not html.xpath("//input[@name='orilink']/@value"):
         raise DirectDownloadLinkException("ERROR: Unable to find Direct Link")
