8a1c1cdfc2d7bf54183b55fb3c9b74464fd74567
tanmayf <fndmydvc0077@gmail.com>
2025-03-16 11:49:14 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -171,8 +171,8 @@ def get_progress_bar_string(pct):
         pct = float(pct.strip("%"))
     p = min(max(pct, 0), 100)
     c_full = int((p + 5) // 10)
-    p_str = "●" * c_full
-    p_str += "○" * (10 - c_full)
+    p_str = "⚪" * c_full
+    p_str += "⚫" * (10 - c_full)
     return p_str
 
 
